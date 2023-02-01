cea03d2862f11af7a10490f9759683ab5e917558
Ahsan Abbasi <1abbasia@gmail.com>
2023-01-31 19:45:13 -0500

    started views file

--- photolibrary/models.py
+++ photolibrary/models.py
@@ -1,6 +1,17 @@
 from django.db import models
-
+from PIL import Image
 # Create your models here.
 class Photo(models.Model):
     image = models.ImageField(upload_to='photos/')
-    uploaded_at = models.DateTimeField(auto_now_add=True)
+    uploaded_at = models.DateTimeField()
+
+    def save(self, *args, **kwargs):
+        super().save(*args, **kwargs)
+
+        img = Image.open(self.image.path)
+        exif_data = img._getexif()
+        if exif_data:
+            for tag, value in exif_data.items():
+                if tag == 36867:
+                    self.uploaded_at = value
+                    self.save()
