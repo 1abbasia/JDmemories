87df7262433302eeca8a2021daecba9b873247d7
Ahsan Abbasi <1abbasia@gmail.com>
2023-02-01 00:38:12 -0500

    code so far with views urls andmodel set up

--- photolibrary/views.py
+++ photolibrary/views.py
@@ -4,4 +4,4 @@ from .models import Photo
 
 def photo_list(request):
     photos = Photo.objects.all()
-    return render(request, 'photos/photo_list.html', {'photos': photos})
+    return render(request, 'photolibrary/photo_list.html', {'photos': photos})
