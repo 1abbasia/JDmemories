20cccb6bdfe34fa9e627bf806fc698c78612fb3c
Ahsan Abbasi <1abbasia@gmail.com>
2023-02-02 18:49:44 -0500

    added AWS keys to settings file

--- JDmemories/settings.py
+++ JDmemories/settings.py
@@ -123,3 +123,10 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+AWS_ACCESS_KEY_ID = "AKIA6FMAPQQLRSNPTKAY"
+AWS_SECRET_ACCESS_KEY = "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"
+AWS_STORAGE_BUCKET_NAME = "jdmemories"
+
+# Use Amazon S3 for storage for uploaded media files.
+DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
